16d25a9ced21368bbf871d156bceb3e1e3ac3eb1
ngo van nguyen <chickie8x@ngos-iMac-Pro.local>
2022-07-08 19:05:17 +0700

    minor fix

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,12 +1,12 @@
 <template>
   <div class=" flex px-4 justify-between">
     <div v-for="day, idx in weatherInfo.weather.forecast.forecastday" :key="idx" class=" p-1">
-      <div class=" flex justify-start text-sm items-center text-white">
-        <span>{{ day.date }}</span>
+      <div class=" flex justify-start text-xs items-center text-white">
         <img :src="day.day.condition.icon" class=" w-[28px] h-[28px]" alt="">
+         <span>{{ day.date }}</span>
       </div>
       <span class="text-xs text-white">{{ day.day.condition.text }}</span>    
-      <div class="text-white text-sm mt-2">
+      <div class="text-white text-xs mt-2">
       {{ day.day.mintemp_c}}°C - {{ day.day.maxtemp_c }}°C
     </div>
     </div>
